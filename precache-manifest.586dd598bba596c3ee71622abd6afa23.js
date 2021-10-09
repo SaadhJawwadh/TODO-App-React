612107f21d45cae058afb60fd5130d134904bab1
Saadh Jawwadh <66717664+SaadhJawwadh@users.noreply.github.com>
2021-10-09 12:01:57 +0530

    Updates

---
+++ precache-manifest.586dd598bba596c3ee71622abd6afa23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66cc92ad6dc345552177c53fc5ff861e",
+    "url": "/TODO-App-React/index.html"
+  },
+  {
+    "revision": "50bdaf733e12db006ea2",
+    "url": "/TODO-App-React/static/css/main.64a7e980.chunk.css"
+  },
+  {
+    "revision": "5960e7b0e64ef3efa2b2",
+    "url": "/TODO-App-React/static/js/2.62368e13.chunk.js"
+  },
+  {
+    "revision": "50bdaf733e12db006ea2",
+    "url": "/TODO-App-React/static/js/main.101972ff.chunk.js"
+  },
+  {
+    "revision": "8eed720958a3a21cb0b9",
+    "url": "/TODO-App-React/static/js/runtime~main.61e645af.js"
+  }
+]);
